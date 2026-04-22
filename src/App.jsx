e53f2bd650c041e7cda766405ced86654bf3994b
Shihab Ahmed <shehabuddin380@gmail.com>
2026-04-22 16:02:19 -0700

    include Navber

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useEffect,useState } from "react"
+import AOS from "aos"
+import "aos/dist/aos.css"
+import Navber from "./components/Navber"
+
 const App=()=>{
+  const [darkMode,setDarkMode]=useState(true)
+  useEffect(()=>{
+    AOS.init({
+      duration:1000,
+      once:false,
+      offset:100
+    })
+    document.documentElement.classList.add('dark');
+  },[]);
+  const toggleDarkMode= ()=>{
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    document.documentElement.classList.toggle('dark');
+  };
   return(
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className={
+      darkMode
+      ? 'bg-linear-to-br from-gray-900 via-gray-900 min-h-screen'
+      : 'bg-linear-to-br from-gray-50 min-h-screen'
+    }>
+       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
     </div>
   )
 }
---
+++ src/components/Navber.jsx
@@ -0,0 +1,164 @@
+import { motion, scale } from "framer-motion";
+import { Moon, Sun } from "lucide-react";
+import { useState } from "react"
+
+const Navber=({ darkMode, toggleDarkMode })=>{
+    const [activeSection,setActiveSection]=useState('home');
+    const [isMenuOpen,setIsMenuOpen]=useState(false);
+    const navIrtems=[
+        {name:'Home',link:'#home'},
+        {name:'About',link:'#about'},
+        {name:'Skills',link:'#skills'},
+        {name:'Projects',link:'#projects'},
+        {name:'Contact',link:'#contact'},
+    ];
+
+    const lightColor={
+        navBg:'bg-linear-to-br from-orange-200 to-white',
+        textPrimary:'text-gray-900',
+        textSecondary:'text-gray-800',
+        textHover:'text-orange-500',
+        textActive:'text-orange-600',
+        indicator:'from-orange-500 to-amber-500',
+        button:'bg-orange-500 to-amber-500',
+    };
+    const darkColors={
+        navBg:'bg-linear-to-br from-gray-700 to-black',
+        textPrimary:'text-white',
+        textSecondary:'text-gray-300',
+        textHover:'text-orange-400',
+        textActive:'text-orange-400',
+        indicator:'from-orange-500 to-amber-500',
+        button:'bg-orange-500 to-amber-500',
+    };
+
+    const colors=darkMode?darkColors:lightColor;
+    const handleNavClick=(section)=>{
+        setActiveSection(itemName.toLowerCase());
+        setIsMenuOpen(false);
+    };
+    return(
+        <div className="flex justify-center w-full fixed z-50 mt-4" >
+            <Motion.nav
+            initial={{y:-100}}
+            animate={{y:0}}
+            transition={{duration:0.5}}
+            className={`flex items-center justify-center ${colors.navBg} backdrop-blur-lg rounded-2xl px-4 lg:px-8 py-2 shadow-lg`}>
+                <dib className="flex items-center justify-between w-full space-x-6 lg:space-x-8">
+                    {/* logo */}
+                    <Motion.a 
+                    href="/" 
+                    whileHover={{scale:1.05}}
+                    className="flex items-center space-x-2">
+                        <span 
+                        className={`text-xl font-bold ${colors.textPrimary}`}>
+                            Portfolio <span className="text-orange-500">.</span>
+
+                        </span>
+                    </Motion.a>
+                    {/* Navigation Items */}
+                    <div className="hidden lg:flex items-center space-x-6">
+                        {navIrtems.map((item) => (
+                            <a key={item.name}
+                               href={item.link}
+                               onClick={()=> handleNavClick(item.name)}  
+                               className="relative"
+                            >                      
+                            <motion.span className={`font-medium transition-colors duration-300
+                                ${
+                                    activeSection === item.name.toLowerCase()
+                                    ? colors.textActive
+                                    : `${colors.textSecondary}hover:text-orange-500`
+                                }`}
+                                
+                                whileHover={{scale:1.05}}
+                                whileTap={{scale:0.95}}>
+                                    {item.name}
+                            </motion.span>  
+                            {activeSection === item.name.toLowerCase()&&(
+                                <motion.div 
+                                layoutId="navbar-indicator"
+                                className={`absolute -bottom-1 left-0 right-0 h-0.5 bg-linear-to-r rounded-full &{colors.indicator}`}>
+
+                                </motion.div>
+                            )}       
+                            </a>
+                        ))}
+                    </div>
+                        <div className="flex items-center space-x-2">
+                            <motion.button
+                            whileHover={{scale:1.1}}
+                            whileTap={{scale:0.9}}
+                            onClick={toggleDarkMode}
+                            className={`p-2 rounded-full ${darkMode ? 'bg-gray-700':bg-gray-200} 
+                            transition-colors`}
+                            aria-label={darkMode 
+                            ? 'Switch to light mode '
+                            : 'Switch to dark mode'
+                            }>
+                                {darkMode ?(
+                                    <Sun className="w-5 h-5 text-yellow-500 "/>
+                                ):(
+                                    <Moon className="w-5 h-5 text-gray-700"/>
+                                )}
+                                </motion.button>
+
+
+                                {/* Button */}
+
+                                <motion.a 
+                                href="#contact"
+                                whileHover={{scale:1.05}}
+                                whileTap={{scale:0.95}}
+
+                                className={`hidden lg:block px-6 py-2 font-semibold rounded-full bg-linear-to-r $ {colors.botton}
+                                    text-white shadow-md hover:shadow-lg transition-shadow `}>
+                                        Hire Me
+                            </motion.a>
+                        </div>
+                        {/* Mobile Button */}
+                        <div className="flex lg:hidden items-center space-x-4 px-2">
+                            <motion.button
+                            whileHover={{scale:0.9}}
+                            onClick={()=> setIsMenuOpen(!isMenuOpen)}
+                            className={`p-2 rounded-lg ${darkMode
+                                ? 'bg-gray-700'
+                                : 'bg-gray-200'
+                            }`}>
+                                {isMenuOpen ?(
+                                    <X className={`w-5 h-5 ${darkMode
+                                        ? 'text-white'
+                                        : 'text-gray-700'
+                                    }`}/>
+                                ):(
+                                    <Menu className={`w-5 h-5 ${darkMode
+                                        ? 'text-white'
+                                        : 'text-gray-700'
+                                    }`}/>
+                                )}
+                            </motion.button>
+                        </div>
+                 </dib>
+                 {isMenuOpen &&(
+                    <motion.div
+                    initial={{opacity:0,height:0}}
+                    animate={{opacity:1,height:'auto'}}
+                    exit={{opacity:0,height:0}}
+                    transition={{duration:0.3}}
+                    className={`absolute top-full left-0 right-0 mt-2 lg:hidden 
+                        ${darkMode
+                            ? 'bg-gray-900/95'
+                            : 'bg-white/95'
+                        }backdrop-blur-lg rounded-xl shadow-lg border ${
+                            darkMode ? 'border-gray-700'
+                            : 'border-gray-200'
+                        }`}>
+
+
+                    </motion.div>
+                 )}
+            </Motion.nav>
+        </div>
+        
+    )
+}
