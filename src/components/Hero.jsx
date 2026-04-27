04ce42d6fbeb4f8d74804f580293fabd0b4f0b31
Shihab Ahmed <shehabuddin380@gmail.com>
2026-04-27 01:00:03 -0700

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -71,7 +71,7 @@ const Hero = ({darkMode}) =>{
                                     </button>
                                 </a>
                                 <a href="#contact" className='w-full sm:w-auto'>
-                                    <button className={`w-full sm:w-auto inline-flex items-center ${themes.buttonSecondary} justify-center text-white bg-linear-to-r from-orange-500 to-amber-500 border-0 py-3 px-6 sm:px-8 hover:shadow-[0_0_40px_rgb(255,165,0,0.7)] rounded-full text-base sm:text-lg font-semibold transition-all duration-300 transform `}>
+                                    <button className={`w-full sm:w-auto inline-flex items-center ${themes.buttonSecondary} justify-center text-white border-0 py-3 px-6 sm:px-8 hover:shadow-[0_0_40px_rgb(255,165,0,0.7)] rounded-full text-base sm:text-lg font-semibold transition-all duration-300 transform `}>
                                         <Mail className='w-4 h-4 sm:h-5 sm:w-5 mr-2'/>
                                         Contact Me
                                     </button>
