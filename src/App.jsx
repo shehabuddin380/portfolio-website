43d57348454ad4515d45c2b0d1988a08465489cf
Shihab Ahmed <shehabuddin380@gmail.com>
2026-04-29 08:42:12 -0700

    update index.css and include Abut file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AOS from "aos"
 import "aos/dist/aos.css"
 import Navber from "./components/Navber"
 import Hero from "./components/Hero"
+import About from "./components/About"
 
 const App=()=>{
   const [darkMode,setDarkMode]=useState(true)
@@ -36,6 +37,7 @@ const App=()=>{
     }>
        <Navber darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
        <Hero darkMode={darkMode} />
+       <About darkMode={darkMode} />
     </div>
   )
 }
