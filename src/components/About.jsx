43a6ca703814dc4765ca8362ab87a6ee42416d05
Shihab Ahmed <shehabuddin380@gmail.com>
2026-04-29 09:12:48 -0700

    update About file and digain

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+
+
+const About =(darkMode)=>{
+    return (
+        <section id="about" className={`min-h-screen overflow-hidden flex items-center justify-center px-4 sm:px-6${
+            darkMode ? 'bg-gray-900' : 'bg-gray-50'
+        }`}>
+            <div className="max-w-6xl w-full grid grid-cols-1 lg:grid-cols-2 gap-8 sm:gap-12 items-center">
+                <figure
+                data-aos="fade-up"
+                data-aos-delay="300"
+                className="flex flex-wrap justify-center gap-4 relative order-2 lg:order-1">
+                    <div className="relative w-75 h-75 lg:w-96 lg:h-96">
+                        <div className=" absolute -inset-4 lg:-inset-20 bg-linear-to-1 from-[#f97316] via-[#fb923c] to-[#f59e0b] rotate-12 star-shape z-0"
+                        data-aos="zoom-in"
+                        data-aos-delay='600'>
+                        </div>
+
+                    </div>
+
+                </figure>
+
+            </div>
+
+        </section>
+    )
+
+}
