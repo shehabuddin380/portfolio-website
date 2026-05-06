ef75c5a2cf531029f5dadebe5152446f4948c4a5
Shihab Ahmed <shehabuddin380@gmail.com>
2026-05-06 09:19:31 -0700

    add projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,45 @@
+
+
+
+const Projects = ()=>{
+    const projects = [
+        {
+            id:1,
+            title:'PhiMart',
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            image:project1,
+            tags:['Python','Django']
+        },
+        {
+            id:2,
+            title:'PhiMart Client',
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            image:project1,
+            tags:['React', 'JavaScript']
+        },
+        {
+            id:3,
+            title:'Hotel Booking',
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            image:project1,
+            tags:['Python','Django']
+        },
+        {
+            id:4,
+            title:'Hotel Frontend',
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            image:project1,
+            tags:['React', 'JavaScript']
+        },
+        {
+            id:5,
+            title:'Task Managment',
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            image:project1,
+            tags:['Python','Django']
+        },
+    return (
+
+    )
+}
+export default Projects;
