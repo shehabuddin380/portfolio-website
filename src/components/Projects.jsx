2d09de10323c91c0c8f3eb8676f24a5ff8881824
Shihab Ahmed <shehabuddin380@gmail.com>
2026-05-09 09:57:24 -0700

    update portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -47,9 +47,9 @@ const Projects = ({darkMode}) => {
         }}
         className='relative py-24'>
             <div className='container mx-auto px-4'>
-                <div className='text-center sm:text-4xl font-bold mb-3'>
+                <div className='text-center mb-10 data-aos="fade-up"'>
                     <h2
-                    className=""
+                    className="text-3xl sm:text-4xl font-bold mb-3"
                     style={{
                         color: darkMode ? 'white' : '#1f2937'
                     }}>
